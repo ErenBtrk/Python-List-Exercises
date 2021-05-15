f3086ed98a678732fb833a8212e7b18bc8e61ded
ErenBtrk <erenbtrk@hotmail.com>
2021-05-16 00:22:46 +0300

    Exercise 12

--- Exercise12.py
+++ Exercise12.py
@@ -19,6 +19,8 @@ while len(del_index) != 0:
 
 print(my_list)
 
+################################################################################################
+
 color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
 color = [x for (i,x) in enumerate(color) if i not in (0,4,5)]
 print(color)
