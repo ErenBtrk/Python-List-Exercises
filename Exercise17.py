8b5cbc55f31f8b5d6554dab1da1171047bfdf146
ErenBtrk <erenbtrk@hotmail.com>
2021-05-16 00:36:08 +0300

    Exercise 17

---
+++ Exercise17.py
@@ -0,0 +1,13 @@
+'''
+17. Write a Python program to generate and print a list except for
+the first 5 elements, where the values are square of numbers between
+1 and 30 (both included).
+'''
+
+list1 = []
+
+for i in range(1,31):
+    list1.append(i**2)
+
+print(list1[5:len(list1)])
+
