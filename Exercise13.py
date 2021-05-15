6a03cf45bde7733d60fa14458cf849feaba3ffde
ErenBtrk <erenbtrk@hotmail.com>
2021-05-16 00:20:06 +0300

    Exercise 13

---
+++ Exercise13.py
@@ -0,0 +1,9 @@
+'''
+13. Write a Python program to generate a 3*4*6 3D array whose
+ each element is *.
+'''
+
+my_list = [[['*' for column in range(6)]for column in range(4)]for row in range(3)]
+
+print(my_list)
+    
