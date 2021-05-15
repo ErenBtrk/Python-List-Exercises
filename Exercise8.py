fbd9b31f33896f5ce6ec9b793fb9d5fe0db2b3a8
ErenBtrk <erenbtrk@hotmail.com>
2021-05-15 23:00:16 +0300

    Exercise 8

---
+++ Exercise8.py
@@ -0,0 +1,11 @@
+'''
+8. Write a Python program to check a list is empty or not.
+'''
+
+my_list1 = []
+my_list2 = [1,2,3]
+
+if(len(my_list1) == 0):
+    print("list1 is empty.")
+if(len(my_list2) == 0):
+    print("list 2 is empty")
