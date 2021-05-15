58d7d8469ac4dca01edb0c1c569ba19530d07a69
ErenBtrk <erenbtrk@hotmail.com>
2021-05-15 22:41:36 +0300

    Exercise 5

---
+++ Exercise5.py
@@ -0,0 +1,16 @@
+''' 
+5.Write a Python program to count the number of strings where the string 
+length is 2 or more and the first and last character are same from a given
+ list of strings.
+Sample List : ['abc', 'xyz', 'aba', '1221']
+Expected Result : 2
+'''
+
+my_list = ['abc', 'xyz', 'aba', '1221']
+count = 0
+
+for item in my_list:
+    if(len(item) >= 2 and item[0]==item[-1]):
+        count += 1
+
+print(count)
